c951eb1ca7e102fb3da0a0ee1176a9b3eb74955d
liushaofei <1652396752@qq.com>
2019-08-19 00:23:52 +0800

    菜单栏及楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,9 @@ Page({
       // 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
       // 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       // 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-    ]
+    ],
+    menu:[],
+    floorData:[]
   },
 
   onLoad() {
@@ -21,6 +23,26 @@ Page({
       // 修改imgUrls
       this.setData({
         imgUrls: message
+      })
+    }),
+
+    request({
+      url:"/home/catitems"
+    }).then(res=>{
+      console.log(res)
+      const {message} = res.data
+      this.setData({
+      menu:message
+      })
+    }),
+    
+    request({
+      url:"/home/floordata"
+    }).then(res=>{
+      console.log( res)
+      const {message} = res.data
+      this.setData({
+        floorData:message
       })
     })
   }
