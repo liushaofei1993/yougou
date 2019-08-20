d442e1ec4bbfb58265b52b0bb852303e01fa1247
liushaofei <1652396752@qq.com>
2019-08-20 21:40:00 +0800

    分类页菜单栏和分类列表静态布局完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,16 +1,48 @@
-// pages/category/index.js
+import request from '../../utils/request'
 Page({
 
   /**
    * 页面的初始数据
    */
-  
+  data:{
+    navs:[
+     "大家电", 
+     "空调", 
+     "冰箱", 
+     "洗衣机", 
+     "好东西", 
+     "电闹办公", 
+     "大家电", 
+     "空调", 
+     "冰箱", 
+     "洗衣机", 
+     "好东西", 
+     "电闹办公", 
+     "大家电", 
+     "空调", 
+     "冰箱", 
+     "洗衣机", 
+     "好东西", 
+     "电闹办公", 
+    ],
+    current:0
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    request({
+      url:"/categories"
+    }).then(res=>{
+      console.log(res)
+    })
   },
-
- 
+  handleGetBrand(event){
+    console.log(event)
+    const {index} = event.currentTarget.dataset
+    this.setData({
+      current:index
+    })
+  }
+  
 })
