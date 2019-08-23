847470d342deb9eefe0c6d812a683a36d85ffa17
liushaofei <1652396752@qq.com>
2019-08-23 21:20:03 +0800

    分类页跳转到搜索页,并正式渲染

--- pages/search_list/index.js
+++ pages/search_list/index.js
@@ -15,9 +15,14 @@ Page({
    */
   onLoad: function (options) {
     // 假设关键字是电视
-    let keyword ="小米"
+    // let keyword ="小米"
+
+    // 获取点击时传过来的参数(options相当于this.$router.query.)赋值给变量keyword
+    this.setData({
+      keyword:options.keyword
+    })
     request({
-      url:"/goods/search?query=" + keyword ,
+      url:"/goods/search?query=" + options.keyword ,
 
     }).then(res=>{
       console.log(res)
@@ -27,8 +32,7 @@ Page({
         return v
       })
       this.setData({
-        goods:newGoods,
-        keyword
+        goods:newGoods
       })
     })
   },
